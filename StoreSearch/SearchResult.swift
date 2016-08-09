4db2c457a56a569eb15fa5469c2cabba97aeeab7
Sean Perez <seanperez@seans-macbook-pro-2.local>
2016-08-09 00:42:54 -0700

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Sean Perez on 8/9/16.
+//  Copyright © 2016 SeanPerez. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
