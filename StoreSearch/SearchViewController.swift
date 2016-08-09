1e1016d8c3d7340673d2bd5f1c61d37ed441d322
Sean Perez <seanperez@seans-mbp-2.attlocal.net>
2016-08-08 21:39:31 -0700

    Renamed ViewController to SearchViewController

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  StoreSearch
 //
 //  Created by Sean Perez on 8/8/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
