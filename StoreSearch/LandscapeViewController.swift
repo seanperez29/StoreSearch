c06124911e6513c5c16e1dd5f163dd83d687ccea
Sean Perez <sean.perez29@gmail.com>
2016-08-21 19:59:21 -0700

    Add conditional 'firstTime' to viewWillLayoutSubviews

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -31,6 +31,11 @@ class LandscapeViewController: UIViewController {
         super.viewWillLayoutSubviews()
         scrollView.frame = view.bounds
         pageControl.frame = CGRect(x: 0, y: view.frame.size.height - pageControl.frame.size.height, width: view.frame.size.width, height: pageControl.frame.size.height)
+        
+        if firstTime {
+            firstTime = false
+            //tileButtons(searchResults)
+        }
     }
     
     deinit {
