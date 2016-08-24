839b60d6f07a2ac4971ad30bb312bbd7672e09f2
Sean Perez <sean.perez29@gmail.com>
2016-08-23 23:10:50 -0700

    Add back 'firstTime' variable and implement first half of 'tileButtons' function

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -31,7 +31,42 @@ class LandscapeViewController: UIViewController {
         super.viewWillLayoutSubviews()
         scrollView.frame = view.bounds
         pageControl.frame = CGRect(x: 0, y: view.frame.size.height - pageControl.frame.size.height, width: view.frame.size.width, height: pageControl.frame.size.height)
+        
+        if firstTime {
+            firstTime = false
+            tileButtons(searchResults)
         }
+    }
+    
+    func tileButtons(searchResults: [SearchResult]) {
+        var columnsPerPage = 5
+        var rowsPerPage = 3
+        var itemWidth: CGFloat = 96
+        var itemHeight: CGFloat = 88
+        var marginX: CGFloat = 0
+        var marginY: CGFloat = 20
+        
+        let scrollViewWidth = scrollView.bounds.size.width
+        
+        switch scrollViewWidth {
+        case 568:
+            columnsPerPage = 6
+            itemWidth = 94
+            marginX = 2
+        case 667:
+            columnsPerPage = 7
+            itemWidth = 95
+            itemHeight = 98
+            marginX = 1
+            marginY = 29
+        case 736:
+            columnsPerPage = 8
+            rowsPerPage = 4
+            itemWidth = 92
+        default:
+            break
+        }
+    }
     
     deinit {
         print("deinit \(self)")
